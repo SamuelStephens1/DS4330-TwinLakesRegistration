2024eae530164a2d4c9f972b0ba42bd1d9cfb048
JacobLinkous <jacobmlinkous@gmail.com>
2024-11-07 09:17:02 -0600

    added adv search

--- Backend API/server.js
+++ Backend API/server.js
@@ -19,20 +19,20 @@ var config = {
     }
 };
 
-// Create the app.get code here 
-// This code should take a get request to ‘/‘ and return a simple message saying ‘There is 1 record in the dataset’
-// Define route to accept 'county' as a parameter
-app.get('/:county', (req, res, next) => {
+//request for simple search
+app.get('/:zip/:phone', (req, res, next) => {
     
     // Close any previous SQL connections
     sql.close();
 
-    // Get the 'county' parameter from the request URL
-    const county = req.params.county;
+    // Get the parameters from the request URL
+    const zip = req.params.zip;
+    const phone = req.params.phone;
 
     // Connect to your database
     sql.connect(config, function (err) {
 
+        //error if no database connection
         if (err) {
             console.log(err);
             res.status(500).send('Database connection error');
@@ -43,9 +43,57 @@ app.get('/:county', (req, res, next) => {
         var request = new sql.Request();
 
         // Use parameterized query to prevent SQL injection
-        const query = "SELECT * FROM data WHERE service_county = @county";
+        const query = "SELECT * FROM data WHERE service_zip = @zip and phone = @phone";
 
-        // Add the county parameter to the SQL query
+        // Add the parameters to the SQL query
+        request.input('zip', sql.VarChar, zip);
+        request.input('phone', sql.VarChar, phone);
+
+        // Execute the query
+        request.query(query, function (err, result) {
+            if (err) {
+                console.log(err);
+                res.status(500).send('Query execution error');
+            } else {
+                res.json(result);
+            }
+
+            // Close the SQL connection
+            sql.close();
+        });
+    });
+});
+
+//request for advanced search
+app.get('/:name/:address/:county', (req, res, next) => {
+    
+    // Close any previous SQL connections
+    sql.close();
+
+    // Get the parameters from the request URL
+    const name = req.params.name;
+    const address = req.params.address;
+    const county = req.params.county;
+
+    // Connect to your database
+    sql.connect(config, function (err) {
+
+        //error if no database connection
+        if (err) {
+            console.log(err);
+            res.status(500).send('Database connection error');
+            return;
+        }
+
+        // Create Request object
+        var request = new sql.Request();
+
+        // Use parameterized query to prevent SQL injection
+        const query = "SELECT * FROM data WHERE name = @name or service_address = @address or service_county = @county";
+
+        // Add the parameters to the SQL query
+        request.input('name', sql.VarChar, name);
+        request.input('address', sql.VarChar, address);
         request.input('county', sql.VarChar, county);
 
         // Execute the query
@@ -60,10 +108,8 @@ app.get('/:county', (req, res, next) => {
             // Close the SQL connection
             sql.close();
         });
-
     });
 });
-
 
 // This code tells the application to listen on the port we set at the top of the code.  Additionally, the console.log command allows us to have a visual representation in our command prompt or terminal telling us the node project is running and which port we should try to access it on. 
 app.listen(port, () => {
